09f2f6dbfa6cd31e55d91aa5bdd090fd9943aeee
Abhishek Walia <abhishek.walia@gmail.com>
2023-05-16 00:19:01 -0700

    move org fetches to the new handler codebase.

--- src/ccloud/org.py
+++ src/ccloud/org.py
@@ -4,12 +4,10 @@ import os
 from typing import Dict, List, Set, Tuple
 import pandas as pd
 from ccloud.connections import CCloudBase, CCloudConnection, EndpointURL
-from ccloud.data_handlers import (
-    CCloudBillingHandler,
-    CCloudChargebackHandler,
-    CCloudMetricsHandler,
-    CCloudObjectsHandler,
-)
+from data_processing.data_handlers.billing_api_handler import CCloudBillingHandler
+from data_processing.data_handlers.ccloud_api_handler import CCloudObjectsHandler
+from data_processing.data_handlers.prom_metrics_handler import PrometheusMetricsDataHandler
+from data_processing.data_handlers.chargeback_handler import CCloudChargebackHandler
 from helpers import sanitize_id
 from storage_mgmt import (
     BILLING_PERSISTENCE_STORE,
@@ -24,47 +22,56 @@ class CCloudOrg:
     in_days_in_memory: InitVar[int] = field(default=7)
     org_id: str
 
-    metrics_handler: CCloudMetricsHandler = field(init=False)
     objects_handler: CCloudObjectsHandler = field(init=False)
+    metrics_handler: PrometheusMetricsDataHandler = field(init=False)
     billing_handler: CCloudBillingHandler = field(init=False)
-    chargeback_handler: CCloudChargebackHandler = field(init=False)
+    # chargeback_handler: CCloudChargebackHandler = field(init=False)
 
     def __post_init__(self, in_org_details, in_days_in_memory) -> None:
         self.org_id = sanitize_id(in_org_details["id"])
         # Initialize the CCloud Objects Handler
         self.objects_handler = CCloudObjectsHandler(
-            ccloud_connection=CCloudConnection(
+            in_ccloud_connection=CCloudConnection(
                 in_api_key=in_org_details["ccloud_details"]["ccloud_api"]["api_key"],
                 in_api_secret=in_org_details["ccloud_details"]["ccloud_api"]["api_secret"],
                 base_url=EndpointURL.API_URL,
-            )
-        )
-
-        # Initialize the Metrics Handler
-        self.metrics_handler = CCloudMetricsHandler(
-            org_id=self.org_id,
-            in_requests=in_org_details["requests"],
-            days_in_memory=in_days_in_memory,
-            cc_objects=self.objects_handler,
-            in_ccloud_connection=CCloudConnection(
-                in_api_key=in_org_details["ccloud_details"]["metrics_api"]["api_key"],
-                in_api_secret=in_org_details["ccloud_details"]["metrics_api"]["api_secret"],
-                base_url=EndpointURL.TELEMETRY_URL,
             ),
         )
 
+        # This start date is calculated from the now time to rewind back 365 days as that is the
+        # time limit of the billing dataset which is available to us. We will need the metrics handler
+        # to try and get the data from that far back as well.
+        start_date = datetime.datetime.now(tzinfo=datetime.timezone.utc).combine(
+            time=datetime.time.min
+        ) - datetime.timedelta(days=365)
+
         # Initialize the Billing API Handler
         self.billing_handler = CCloudBillingHandler(
-            org_id=self.org_id,
-            in_ccloud_api_connection=CCloudConnection(
+            in_ccloud_connection=CCloudConnection(
                 in_api_key=in_org_details["ccloud_details"]["billing_api"]["api_key"],
                 in_api_secret=in_org_details["ccloud_details"]["billing_api"]["api_secret"],
                 base_url=EndpointURL.API_URL,
             ),
+            start_date=start_date,
+        )
+
+        # Initialize the Metrics Handler
+        self.metrics_handler = PrometheusMetricsDataHandler(
+            in_ccloud_connection=CCloudConnection(
+                in_api_key=in_org_details["ccloud_details"]["metrics_api"]["api_key"],
+                in_api_secret=in_org_details["ccloud_details"]["metrics_api"]["api_secret"],
+            ),
+            in_prometheus_url="http://localhost:9090",
+            start_date=start_date,
         )
 
         # Initialize the Chargeback Object Handler
-        self.chargeback_handler = CCloudChargebackHandler(org_id=self.org_id, cc_objects=self.objects_handler)
+        self.chargeback_handler = CCloudChargebackHandler(
+            billing_dataset=self.billing_handler,
+            objects_dataset=self.objects_handler,
+            metrics_dataset=self.metrics_handler,
+            start_date=start_date,
+        )
 
     def execute_requests(self):
         print(f"Gathering CCloud Existing Objects Data")
@@ -75,35 +82,37 @@ class CCloudOrg:
         )
         print(f"Checking for new Billing CSV Files")
         self.billing_handler.execute_requests()
+        print("Calculating next dataset for chargeback")
+        self.chargeback_handler.execute_requests()
 
-    def find_available_hour_slices_in_billing_datasets(self) -> datetime.datetime:
-        for item in self.billing_handler.available_hour_slices_in_dataset:
-            yield datetime.datetime.fromisoformat(item)
+    # def find_available_hour_slices_in_billing_datasets(self) -> datetime.datetime:
+    #     for item in self.billing_handler.available_hour_slices_in_dataset:
+    #         yield datetime.datetime.fromisoformat(item)
 
-    def run_calculations(self):
-        curr_date = datetime.datetime.now(tz=None).replace(hour=0, minute=0, second=0, microsecond=0)
-        for hour_slice in self.find_available_hour_slices_in_billing_datasets():
-            if hour_slice >= curr_date:
-                print(f"Skipping Billing Row TS - {hour_slice} as the Metrics are fetched with 1 day delay")
-                continue
-            billing_data = self.billing_handler.get_hourly_dataset(time_slice=hour_slice)
-            if billing_data:
-                metrics_found, metrics_data = self.metrics_handler.get_hourly_dataset(
-                    time_slice=hour_slice, billing_mgmt=True
-                )
-                for filename, data_set in billing_data.items():
-                    if not data_set.empty:
-                        self.chargeback_handler.run_calculations(
-                            time_slice=hour_slice, billing_dataframe=data_set, metrics_dataframe=metrics_data,
-                        )
-                        BILLING_PERSISTENCE_STORE.add_data_to_persistence_store(
-                            org_id=self.org_id, key=(hour_slice,), value=filename
-                        )
-            # TODO: Need to add more status for when data is missing, cannot silently ignore.
-            # Bad user experience otherwise.
-        self.chargeback_handler.export_metrics_to_csv(
-            output_basepath=STORAGE_PATH.get_path(org_id=self.org_id, dir_type=DirType.OutputData, ensure_exists=True)
-        )
+    # def run_calculations(self):
+    #     curr_date = datetime.datetime.now(tz=None).replace(hour=0, minute=0, second=0, microsecond=0)
+    #     for hour_slice in self.find_available_hour_slices_in_billing_datasets():
+    #         if hour_slice >= curr_date:
+    #             print(f"Skipping Billing Row TS - {hour_slice} as the Metrics are fetched with 1 day delay")
+    #             continue
+    #         billing_data = self.billing_handler.get_hourly_dataset(time_slice=hour_slice)
+    #         if billing_data:
+    #             metrics_found, metrics_data = self.metrics_handler.get_hourly_dataset(
+    #                 time_slice=hour_slice, billing_mgmt=True
+    #             )
+    #             for filename, data_set in billing_data.items():
+    #                 if not data_set.empty:
+    #                     self.chargeback_handler.run_calculations(
+    #                         time_slice=hour_slice, billing_dataframe=data_set, metrics_dataframe=metrics_data,
+    #                     )
+    #                     BILLING_PERSISTENCE_STORE.add_data_to_persistence_store(
+    #                         org_id=self.org_id, key=(hour_slice,), value=filename
+    #                     )
+    #         # TODO: Need to add more status for when data is missing, cannot silently ignore.
+    #         # Bad user experience otherwise.
+    #     self.chargeback_handler.export_metrics_to_csv(
+    #         output_basepath=STORAGE_PATH.get_path(org_id=self.org_id, dir_type=DirType.OutputData, ensure_exists=True)
+    #     )
 
 
 @dataclass(kw_only=True)
