1b0471818bb01be247e44d52d6486daa1a33ff4c
Abhishek Walia <abhishek.walia@gmail.com>
2023-06-23 16:16:49 -0700

    add scrape status markers for maintaining state in prom

--- src/ccloud/org.py
+++ src/ccloud/org.py
@@ -9,9 +9,17 @@ from ccloud.connections import CCloudConnection, EndpointURL
 from data_processing.data_handlers.billing_api_handler import CCloudBillingHandler
 from data_processing.data_handlers.ccloud_api_handler import CCloudObjectsHandler
 from data_processing.data_handlers.chargeback_handler import CCloudChargebackHandler
-from data_processing.data_handlers.prom_metrics_handler import PrometheusMetricsDataHandler
+from data_processing.data_handlers.prom_metrics_api_handler import PrometheusMetricsDataHandler
 from helpers import sanitize_id
+from prometheus_processing.custom_collector import TimestampedCollector
 from prometheus_processing.notifier import NotifierAbstract, Observer
+
+scrape_status_metrics = TimestampedCollector(
+    "confluent_cloud_custom_scrape_status",
+    "CCloud Scrape Status for various object types",
+    ["object_type"],
+    in_begin_timestamp=datetime.datetime.now(),
+)
 
 
 @dataclass(kw_only=True)
@@ -35,7 +43,7 @@ class CCloudOrg(Observer):
         # to try and get the data from that far back as well.
         start_date = datetime.datetime.utcnow().replace(
             minute=0, second=0, microsecond=0, tzinfo=datetime.timezone.utc
-        ) - datetime.timedelta(days=30)
+        ) + datetime.timedelta(days=-30, hours=+1)
 
         self.exposed_end_date = datetime.datetime.utcnow().replace(
             hour=0, minute=0, second=0, microsecond=0, tzinfo=datetime.timezone.utc
@@ -79,11 +87,11 @@ class CCloudOrg(Observer):
             start_date=start_date,
         )
 
-        self.attach(notifier=self.chargeback_handler.metrics_collector)
+        self.attach(notifier=scrape_status_metrics)
         # This following action is required as for the first run we need to derive the start date.
         # So we step back by 1 hour, so that the current hour slice is returned.
         self.exposed_metrics_datetime = start_date - datetime.timedelta(hours=1)
-        self.update(notifier=self.chargeback_handler.metrics_collector)
+        self.update(notifier=scrape_status_metrics)
 
     def update(self, notifier: NotifierAbstract):
         self.exposed_end_date = datetime.datetime.utcnow().replace(
@@ -91,17 +99,20 @@ class CCloudOrg(Observer):
         ) - datetime.timedelta(days=1)
         next_ts = self._generate_next_timestamp(curr_date=self.exposed_metrics_datetime)
         next_ts_in_dt = next_ts.to_pydatetime(warn=False)
-        if next_ts_in_dt < self.exposed_end_date:
+        if next_ts_in_dt <= self.exposed_end_date:
+            scrape_status_metrics.clear()
             notifier.set_timestamp(curr_timestamp=next_ts_in_dt)
             # self.expose_prometheus_metrics(ts_filter=next_ts)
             print(f"Refreshing CCloud Existing Objects Data")
             self.objects_handler.execute_requests(exposed_timestamp=next_ts_in_dt)
+            notifier.labels("ccloud_objects").set(1)
             print(f"Gathering Metrics API Data")
             self.metrics_handler.execute_requests(exposed_timestamp=next_ts_in_dt)
             print(f"Checking for new Billing CSV Files")
             self.billing_handler.execute_requests(exposed_timestamp=next_ts_in_dt)
             print("Calculating next dataset for chargeback")
             self.chargeback_handler.execute_requests(exposed_timestamp=next_ts_in_dt)
+            notifier.labels("billing_chargeback").set(1)
 
             self.exposed_metrics_datetime = next_ts_in_dt
         else:
