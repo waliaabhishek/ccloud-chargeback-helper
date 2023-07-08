ef08c8784aa4999263bf967f5835772483aa9ae5
Abhishek Walia <abhishek.walia@gmail.com>
2023-07-08 01:22:14 -0700

    format update by black

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from workflow_runner import execute_workflow
 import argparse
 
-parser = argparse.ArgumentParser(description="Command line arguments for controlling the application", add_help=True,)
+parser = argparse.ArgumentParser(
+    description="Command line arguments for controlling the application",
+    add_help=True,
+)
 
 wf_args = parser.add_argument_group("workflow-args", "Workflow Selection Arguments")
 wf_args.add_argument(
