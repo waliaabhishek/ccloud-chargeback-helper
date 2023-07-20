e205e83957ea2c391410a25f9bd22adaf5012232
Abhishek Walia <abhishek.walia@gmail.com>
2023-07-19 17:18:03 -0700

    Running Version

--- deployables/assets/prometheus_for_chargeback/collector.sh
+++ deployables/assets/prometheus_for_chargeback/collector.sh
@@ -4,20 +4,22 @@
 READINESS_PROBE="/is_ready"
 CURRENT_TS_PROBE="/current_timestamp"
 
-READINESS_URL="${CHARGEBACK_READINESS_PROBE}${READINESS_PROBE}"
-TS_URL="${CHARGEBACK_READINESS_PROBE}${CURRENT_TS_PROBE}"
+READINESS_URL="${CHARGEBACK_READINESS_PROBE_URL}${READINESS_PROBE}"
+TS_URL="${CHARGEBACK_READINESS_PROBE_URL}${CURRENT_TS_PROBE}"
 
 SCRAPE_URL="${CHARGEBACK_METRICS_URL}"
 
 check_readiness () {
     # This function checks if the readiness probe is True
     # If it is not, it will wait 5 seconds and try again
-    test=`wget -O - -q ${READINESS_URL} 2>&1 | cut -d ' ' -f 1 `
+    test=`wget -O - -q ${READINESS_URL} 2>&1 | cut -d ' ' -f 1`
     echo "Readiness probe is ${test}"
     while [ ${test} != "True" ]
     do
+        test=`wget -O - -q ${READINESS_URL} 2>&1 | cut -d ' ' -f 1`
+        echo "Readiness probe is ${test}"
         echo "Waiting for readiness probe to be True"
-        sleep 5
+        sleep 3
     done
 }
 
@@ -25,7 +27,7 @@ check_ts_vicinity () {
     # This function checks if the scrape timestamp is getting close to the current time
     # If it is, it will increase the scrape interval to 10 minutes
     # If it is not, it will set the scrape interval to 0.1 seconds
-    TS_VALUE=`wget -O - -q ${TS_URL} 2>&1 | cut -d ' ' -f 1 `
+    TS_VALUE=`wget -O - -q ${TS_URL} 2>&1 | cut -d ' ' -f 1`
     VICINITY_CUTOFF=$(( `date '+%s'` - $(( 24 * 60 * 60 * 5 )) ))
     if [ ${TS_VALUE} -gt ${VICINITY_CUTOFF} ]
     then
@@ -53,10 +55,10 @@ do
     echo "Scraping Interval set to ${SCRAPE_INTERVAL}"
     rm -f index.html index2.html
     wget -T 60 ${SCRAPE_URL}
-    tail +19 index.html > index2.html
+    tail +37 index.html > index2.html
     echo "# EOF" >> index2.html
     promtool tsdb create-blocks-from openmetrics index2.html .
-    rm -f index.html index2.html
+    # rm -f index.html index2.html
     echo "Sleeping for ${SCRAPE_INTERVAL} seconds"
     sleep ${SCRAPE_INTERVAL}
 done
