4750f57d4d4b68c10c47497201697fc38665b681
Abhishek Walia <abhishek.walia@gmail.com>
2024-07-01 02:03:58 -0700

    Adding ENV based config support for backward compatibility with v1. THIS WILL NOT SUPPORT NEWER FEATURES. IT WILL BE EASIER TO MOVE TO THE YAML style config sooner than later and rip the band aid once.

--- main.py
+++ main.py
@@ -19,11 +19,23 @@ wf_args.add_argument(
     help="Provide the path to the config file. Default is ./configs/config_with_no_env_vars.yaml",
 )
 
+wf_args.add_argument(
+    "--env-file",
+    type=str,
+    required=False,
+    help="Provide the path to ENV file for ease of env variable handling. Default is None and discouraged as it is not scalable",
+)
+
+
 arg_flags = parser.parse_args()
 
 # Load dev.env file if in development mode
 if os.getenv("IS_DEV", "False") == "True":
     dotenv.load_dotenv(dotenv.find_dotenv("dev.env"))
+
+# Load the env file if provided
+if arg_flags.env_file:
+    dotenv.load_dotenv(dotenv.find_dotenv(arg_flags.env_file))
 
 # Set the log level based on the environment variable
 if os.getenv("DEBUG", "False") == "True":
